a252ad5e734febaa07659f3cc61214a89d23d0aa
Aleksey Perfilev <perf.yar@gmail.com>
2022-12-05 19:50:21 +0300

    second exercise

--- js/script.js
+++ js/script.js
@@ -14,31 +14,90 @@
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
 
-const adv = document.querySelectorAll('.promo__adv img'),
-      genre = document.querySelector('.promo__genre'),
-      backgroundFilm = document.querySelector('.promo__bg'),
-      listViewedFilms = document.querySelector('.promo__interactive-list');
+    const adv = document.querySelectorAll('.promo__adv img'),
+        backgroundFilm = document.querySelector('.promo__bg'),
+        genre = backgroundFilm.querySelector('.promo__genre'),     
+        listViewedFilms = document.querySelector('.promo__interactive-list'),
+        formAdd = document.querySelector('form.add'),
+        inputFormAdd = formAdd.querySelector('.adding__input'),
+        checkForm = formAdd.querySelector('[type="checkbox"]');
 
-adv.forEach(item => {
-    item.remove();
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        })
+    }   
+    
+    const makeChanges = () => {
+        genre.textContent = 'Драма';
+        backgroundFilm.style.backgroundImage = 'url("../img/bg.jpg")';
+    }
+
+    const sortArr = (arr) => {
+        arr.sort()
+    }
+
+    function showFilm(films, parent) {
+        parent.innerHTML = '';
+        sortArr(films);
+
+        films.forEach((item, i) => {
+            parent.innerHTML += `<li class="promo__interactive-item">${i + 1} ${item}
+                                        <div class="delete"></div>
+                                    </li>`
+        });   
+        
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                showFilm(films, parent)
+            })
+        });
+    }
+    makeChanges();
+    deleteAdv(adv);
+    showFilm(movieDB.movies, listViewedFilms);
+
+
+
+    /* Задания на урок:
+    1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+    новый фильм добавляется в список. Страница не должна перезагружаться.
+    Новый фильм должен добавляться в movieDB.movies.
+    Для получения доступа к значению input - обращаемся к нему как input.value;
+    P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+    2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+    3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+    4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+    "Добавляем любимый фильм"
+    5) Фильмы должны быть отсортированы по алфавиту */
+
+    formAdd.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let newWachedFilm = inputFormAdd.value;
+        if (newWachedFilm) {
+            if (inputFormAdd.value.length > 21) {
+                newWachedFilm = `${newWachedFilm.substring(0, 22)}...`;
+            }
+            movieDB.movies.push(newWachedFilm);
+            showFilm(movieDB.movies, listViewedFilms);
+
+            if (checkForm.checked) {
+                console.log("Добавляем любимый фильм");
+            }
+        }
+        e.target.reset();
+    });
 })
-
-genre.textContent = 'Драма';
-backgroundFilm.style.backgroundImage = 'url(../img/bg.jpg)'
-listViewedFilms.innerHTML = '';
-movieDB.movies.sort();
-movieDB.movies.forEach((item, i) => {
-    listViewedFilms.innerHTML += `<li class="promo__interactive-item">${i + 1} ${item}
-                                    <div class="delete"></div>
-                                  </li>`
-});
